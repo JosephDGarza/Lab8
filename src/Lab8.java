7065c965d45b51542dbd468dc9fc4fa219b738bd
JosephDGarza <32659326+JosephDGarza@users.noreply.github.com>
2017-10-31 22:53:22 -0400

    Update 1

--- src/Lab8.java
+++ src/Lab8.java
@@ -14,64 +14,26 @@ public class Lab8 {
 		String suffix = "";
 		String userContinue = "Y";
 		int j = 0;
-		
 		System.out.println("How many batters will there be?");
-		while (userContinue.equalsIgnoreCase("y")) {
-			System.out.println("Enter number of times at bat: ");
-			int timesAtBat = scnr.nextInt();
-			double[] bases = new double[timesAtBat];
-			scnr.nextLine();
-			
-			for (int i =0; i < bases.length; i ++) {
-				if (i+1 == 1) {
-					suffix = "st";
-				}
-				if (i+1%10 == 2) { // this will remain accurate until the teens position, would need to create an additional if statement. 
-					suffix = "nd";
-					
-				}
-				if (i+1%10 == 3) {
-					suffix = "rd";
-					
-				}
-				if (i+1 > 3) { // this breaks everything over 3 from happening. 
-					suffix = "th";
-				}
-				
-					
-				System.out.println("Now enter your results at bat for the " + (i+1) + suffix + " time at bat");
-				bases[i] = scnr.nextInt();
-				
-				sluggingAvg += bases[i];
-				
-				if (bases[i] > 0) {
-					
-					j++;
-				}
-
-			}
-			
-			
-			System.out.println(j);
-			sluggingAvg = sluggingAvg/bases.length;
-			System.out.println(sluggingAvg);
-			
-			System.out.println("Would you like to add another batter? (Y/N)");
-			userContinue = scnr.next();
-			
-			
-			
-			
-			
-			
-			
-			
+		int batters = scnr.nextInt();
+		Methods.baseball(batters);
 		
-		}
-System.out.println("goodbye");
+		
+		
+			
+			
+			
+			
+			
+			
+			
+			
+		System.out.println("goodbye");
 
-scnr.close();
+		scnr.close();
+		}
+
 	}
 	
 
-}
+
--- src/Methods.java
+++ src/Methods.java
@@ -1,47 +1,95 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Methods {
 
-public static void baseball(double[] player, double[]) {
+public static void baseball(int numPlayers) {
+	
+	
 	String suffix = " ";
 	Scanner scnr = new Scanner(System.in);
 	double sluggingAvg = 0;
 	double j = 0;
+	int numBats = 0;
+	int playerId = 0;
+	int i = 0;
+	int i2 = 0;
+	int k = 0;
+	System.out.println("How many bats will we average out for our players?");
+	numBats = scnr.nextInt();
+	int[][] playersBats = new int [numPlayers][numBats];
 	
-	for (int i =0; i < avgCalc.length; i ++) {
-		if (i+1 == 1) {
-		suffix = "st";
-		}
-		if (i+1%10 == 2) { // this will remain accurate until the teens position, would need to create an additional if statement. 
-			suffix = "nd";
-			
-		}
-		if (i+1%10 == 3) {
-			suffix = "rd";
-			
-		}
-		if (i+1 > 3) { // this breaks everything over 3 from happening. 
-			suffix = "th";
-		}
+	
+	for (i = 0; i < numPlayers; i++) {
+		System.out.println("Enter numbers for your batters");
+		playerId = scnr.nextInt();
+		playersBats[i][0] = playerId;	
 		
-			
-		System.out.println("Now enter your results at bat for the " + (i+1) + suffix + " time at bat");
-		avgCalc[i] = scnr.nextInt();
-		
-		sluggingAvg += avgCalc[i];
-		
-		if (avgCalc[i] > 0) {
-			
-			j++;
-		}
-
 	}
-	System.out.println(j);
-	sluggingAvg = sluggingAvg/avgCalc.length;
-	System.out.println(sluggingAvg);
+	for (i =0; i < numPlayers; i++)
+	{
+		i2 = 0;
+		while (i2 < numBats)
+		{
+			
+			
+			
+			System.out.println("input bats");
+			k = scnr.nextInt();
+			playersBats[i][i2] =k;
+			i2++;
+		}
+	}
 	
-	return;
+	
+	System.out.println(playersBats[0][2]);
+	System.out.println(playersBats[1][2]);
 		
+		
+		
+		
+//	for (int i =0; i < arr1.length; i++) {
+//		if (i+1 == 1) {
+//		suffix = "st";
+//		}
+//		if (i+1%10 == 2) { // this will remain accurate until the teens position, would need to create an additional if statement. 
+//			suffix = "nd";
+//			
+//		}
+//		if (i+1%10 == 3) {
+//			suffix = "rd";
+//			
+//		}
+//		if (i+1 > 3) { // this breaks everything over 3 from happening. 
+//			suffix = "th";
+//		}
+//		
+//			
+//		System.out.println("Now enter your results at bat for the " + (i+1) + suffix + " time at bat");
+//		arr1[playerNumber][i] = scnr.nextInt();
+//		
+//		sluggingAvg += arr1[playerNumber][i];
+//		
+//		if (arr1[playerNumber][i] > 0) {
+//			
+//			j++;
+//		}
+//
+//	}
+//	System.out.println(j);
+//	sluggingAvg = sluggingAvg/arr1.length;
+//	System.out.println(sluggingAvg);
+	}
+	
+public static int forLooping(int z) {
+	for (int x = 0; x < z; x++) {
+		
+	}
+	
+	
+	
+	return z;
 }
 
 public static void main(String[] args) {
